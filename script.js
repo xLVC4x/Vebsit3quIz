d9a0056303fc3a018ef14b05c0bc7f81dab2ed2e
xLVC4x <luca.roselli1996@gmail.com>
2026-07-15 15:42:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const fragen=[
-{titel:"Frage 1",frage:"Luca: /n Wo sind wir auf dem Bild?",antwort:"csd",hintergrund:"bilder/bild1.jpg"},
+{titel:"Frage 1",frage:"Luca: \n Wo sind wir auf dem Bild?",antwort:"csd",hintergrund:"bilder/bild1.jpg"},
 {titel:"Frage 2",frage:"Wer ist hier gesucht?",antwort:"mila",hintergrund:"bilder/bild2.jpg"},
-{titel:"Frage 3",frage:"Ann: /n Frage?",antwort:"Ann",hintergrund:"bilder/bild3.jpg"},
-{titel:"Frage 4",frage:"Timo: /n Mag ich lieber Kuchen oder Torte?",antwort:"beides",hintergrund:"bilder/bild4.jpg"},
-{titel:"Frage 5",frage:"Lilly: /n Auf welchem Festival in welchem Jahr haben wir uns kennengelernt  ?",antwort:"Wacken 2017",hintergrund:"bilder/bild5.jpg"},
+{titel:"Frage 3",frage:"Ann: \n Frage?",antwort:"Ann",hintergrund:"bilder/bild3.jpg"},
+{titel:"Frage 4",frage:"Timo: \n Mag ich lieber Kuchen oder Torte?",antwort:"beides",hintergrund:"bilder/bild4.jpg"},
+{titel:"Frage 5",frage:"Lilly:\n Auf welchem Festival in welchem Jahr haben wir uns kennengelernt  ?",antwort:"Wacken 2017",hintergrund:"bilder/bild5.jpg"},
 {titel:"Frage 6",frage:"6",antwort:"6",hintergrund:"bilder/bild6.jpg"},
 ];
 let i=0;
